425048cd9315861f1db74fefb30f582624723c76
Umar Adeyemi ADEKOLA <adekola.au@gmail.com>
2023-10-20 09:57:43 +0100

    Update Task and Result.sql

--- SQL Challenge by Techavilly/Task and Result.sql
+++ SQL Challenge by Techavilly/Task and Result.sql
@@ -1,14 +1,11 @@
-# Techavilly SQL Challenge Journey
+TECHAVILLY SQL CHALLENGE JOURNEY
 
-**Introduction**
+INTRODUCTION
 
-I am excited to embark on this 30-day SQL challenge organized by Techavilly. Over the next month, I'll be tackling daily SQL questions, striving to find elegant and efficient solutions, and sharing my queries along with the results with all of you.
-
-SQL is a powerful tool for managing and querying data, and I believe this challenge will be a great opportunity to sharpen my skills and learn from the diverse community of participants here on Techavilly.
-
+I am excited to embark on this 30-day SQL challenge organized by Techavilly. Over the next month, I'll be tackling daily SQL questions, striving to find elegant and efficient solutions, and sharing my queries along with the results with all of you. SQL is a powerful tool for managing and querying data, and I believe this challenge will be a great opportunity to sharpen my skills and learn from the diverse community of participants here on Techavilly.
 Let's dive in and make the most of this challenge together! 🚀
 
-**Day 1** -- Using the Movie Data, write a query to show the titles and movies released in 2017 whose vote count is more than 15 and runtime is more than 100
+Day 1 -- Using the Movie Data, write a query to show the titles and movies released in 2017 whose vote count is more than 15 and runtime is more than 100
 
 SELECT 
 	original_title, release_date
@@ -21,10 +18,9 @@ WHERE
 
 ![Screenshot 2023-10-09 155326](https://github.com/Adekolaau/Polished-data-hub/assets/128713981/f6411548-a779-4a24-abf3-a5e6852fbda3)
 
-**_Approach:_**
+APPROACH:
 
-**SELECT** Statement: Begin by specifying what needs to be displayed, which, in this case, is the 'titles' and 'movies' that meet the given criteria.
-
+SELECT Statement: Begin by specifying what needs to be displayed, which, in this case, is the 'titles' and 'movies' that meet the given criteria.
 **FROM** Statement: Use the FROM statement to identify the source table, which is the "Movie Data" table in the "Movies" database.
 
 **WHERE** Statement: Apply filtering conditions using the WHERE statement. Break down the WHERE statement into multiple conditions to ensure that only the desired results are returned:
